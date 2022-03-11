fdac21270864091fc466be4557b3076dadf30c69
Corb2346 <brayan_seyer@hotmail.com>
2022-03-11 13:49:59 -0600

    se añadieron comentario

--- findTheOldest.js
+++ findTheOldest.js
@@ -1,45 +1,45 @@
 
 
-const findTheOldest = function(array) {
+const findTheOldest = function(array) { //funcion que recibe el arreglo que contiene los datos de las personas
 
-let arregloNacimientos = array.map(function(nacimiento){
+let arregloNacimientos = array.map(function(nacimiento){ // funcion que mapea el arreglo principal y obtiene las fechas de nacimiento
 
-  return nacimiento.yearOfBirth;
+  return nacimiento.yearOfBirth; //regresa las fechas de nacimiento de cada objeto
 
 });
-console.log(arregloNacimientos);
+console.log(arregloNacimientos); //imprime el arreglo con las fechas de nacimiento
 
 
-let arregloMuertes = array.map(function(muerte){
+let arregloMuertes = array.map(function(muerte){//crea un arreglo con las fechas de fallecimiento 
 
 
-  return muerte.yearOfDeath;
+  return muerte.yearOfDeath; //mapea y devuelve en un arreglo nuevo las fechas de nacimiento
 
-  });     
+  });    
 
-console.log(arregloMuertes);
+console.log(arregloMuertes); //imprime el arreglo nuevo con las fechas de fallecimiento
 
-let diferenciaFecha = new Array();
+let diferenciaFecha = new Array(); // crea un nuevo arreglo vacia con la diferencia de las fechas
 
-for(let i = 0;i<array.length;i++){
+for(let i = 0;i<array.length;i++){ //recorre el arreglo principal 
 
-  diferenciaFecha[i] = arregloMuertes[i]-arregloNacimientos[i]; 
+  diferenciaFecha[i] = arregloMuertes[i]-arregloNacimientos[i]; //resta las fechas de nacimiento y defuncion y coloca en u nnuevo arreglo
 
-  if(arregloMuertes[i] == void(0)){
-    let yearActual = 0;
-   fechActual = new Date();
-   arregloMuertes[i] = fechActual.getFullYear();
-    console.log(arregloMuertes[i]);
-    console.log(typeof arregloMuertes[i]);
+  if(arregloMuertes[i] == void(0)){ //verifica si el arreglo tiene objetos undefined
+    let yearActual = 0; 
+   fechActual = new Date(); //funcion que obtiene la fecha actual
+   arregloMuertes[i] = fechActual.getFullYear(); // se introduce en arreglo faltante el año actual
+    console.log(arregloMuertes[i]); //imprime año actual 
+    console.log(typeof arregloMuertes[i]); //muestra que tipo de dato es
 
-    diferenciaFecha[i] = arregloMuertes[i]-arregloNacimientos[i]; 
+    diferenciaFecha[i] = arregloMuertes[i]-arregloNacimientos[i]; // si hay undefined hace la diferencia con la fecha actual - fecha de nacimiento
     
   };
 }
 
-console.log(diferenciaFecha);
+console.log(diferenciaFecha); 
 
-const masEdad = diferenciaFecha.reduce(function(a,b){
+const masEdad = diferenciaFecha.reduce(function(a,b){ // funcion que obtiene el numero máximo de un arreglo
 
   let maxNum = Math.max(a,b)
   console.log(array);
@@ -47,16 +47,16 @@ const masEdad = diferenciaFecha.reduce(function(a,b){
   console.log(typeof maxNum)
   return maxNum;
 
-  });
+  }); 
 
-  console.log(masEdad);
+  console.log(masEdad); //imprime el maximo numero del arreglo
 
   let verificador =0;
   
-  for(let i =0;i<diferenciaFecha.length;i++){
+  for(let i =0;i<diferenciaFecha.length;i++){ // ciclo que recorre la diferencia fecha y lo compara con el dato del maximo valor 
 
       verificador =diferenciaFecha[i];
-      if(verificador == masEdad){
+      if(verificador == masEdad){ // una vez que lo encuentra imprime el contenido del indice del arreglo principal en este caso de people. 
 
           console.log(array[i]);
           return array[i]
